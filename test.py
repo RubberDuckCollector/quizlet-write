fc1db6ec98f88db06000d7761ecab2a38fb50f27
Luna Long <eggsim49@gmail.com>
2024-02-24 00:48:23 +0000

    i am literally a genius

--- test.py
+++ test.py
@@ -9,20 +9,35 @@ def parse_msg(msg: str) -> str:
 
     inside_brackets = False
 
-    for i in range(len(msg)):
-        for j in range(len(msg[i])):
-            if msg[i][j] == '(':
-                new_msg += msg[i]
-            elif msg[i][j] in chars_to_ignore:
-                new_msg += msg[i][j]
-            else:
-                new_msg += '_'
+    try:
+        for i in range(len(msg)):
+            if i > 0:
+                new_msg += " "
+                print(
+                    "add a space, i has incremented so that must mean we're on the next word")
+            for j in range(len(msg[i])):
+                print(f"looking at char: {msg[i][j]}")
+                if msg[i][j] == '(':
+                    print("open bracket detected, add all of it to the new msg")
+                    new_msg += msg[i]
+                    i += 1  # since the bracket stuff is done all at once, need to go to next i
+                elif msg[i][j] in chars_to_ignore:
+                    print(
+                        f"want to ignore making this char a _, append it as-is instead")
+                    new_msg += msg[i][j]
+                else:
+                    new_msg += '_'
+
+                print(f"new msg: {new_msg}")
+
+    except IndexError:
+        pass
 
     return new_msg
 
 
 def main():
-    msg = "die Gemeinschaft (-en)"
+    msg = "die Gemeinschaft+ (-en)"
 
     print(parse_msg(msg))
 
