1b728a61c3661b1d1a9dfa4d925f452c1a375afc
Luna Long <eggsim49@gmail.com>
2026-06-29 23:41:38 +0100

    rust rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use console::style;
+use std::{thread, time};
+
+fn clear_screen() {
+    // std::process::Command::new("clear");
+    clearscreen::clear().expect("failed to clear screen");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // clear_screen();
+
+    let ten_millis = time::Duration::from_millis(10);
+    let now = time::Instant::now();
+
+    thread::sleep(ten_millis);
+
+    assert!(now.elapsed() >= ten_millis);
 }
